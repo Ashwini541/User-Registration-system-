7838f6fb1f73c4805126055a8ed92a6afec10ceb
Ashwini541 <ashwini.j@marutsoft.com>
2023-10-26 21:41:58 +0530

    Initial commit

---
+++ group.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 26, 2023 at 05:07 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `group`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_form`
+--
+
+CREATE TABLE `user_form` (
+  `id` int(50) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_form`
+--
+
+INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `image`) VALUES
+(1, 'Ashwini', 'ash@email.com', '81dc9bdb52d04dc20036dbd8313ed055', 'pic.jpg'),
+(2, 'joshi', 'joshi@email.com', '45017f6511f91be700fda3d118034994', 'pic1.jpg'),
+(3, 'aisha', 'aisha@email.com', '81dc9bdb52d04dc20036dbd8313ed055', 'pic2.jpg'),
+(4, 'arya', 'arya@email.com', '827ccb0eea8a706c4c34a16891f84e7b', 'pic3.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_form`
+--
+ALTER TABLE `user_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_form`
+--
+ALTER TABLE `user_form`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
